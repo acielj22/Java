b636d3e03bb0ea5ece1cf88b74aa025d07753308
acielj22 <acielj@nycstudents.net>
2025-09-11 12:29:56 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,7 +1,33 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello World");
-	System.out.println("----- ** -----");
-	System.out.println("Line1\nLine2");
+    (new Main()).init();
   }
+  // This is a single line comment in Java
+  // Multi-line comments are enclosed within  /*  and  */
+  
+  /* 
+  Init function which runs when 
+  the program begins is invoked(called) from within the Main function.
+  */
+  void init(){
+    // print Welcome
+    
+
+    // print Hello with the quotes displayed
+    
+    
+    // print \Java Code\ as shown
+    
+    
+    // print the name Joe tabbed once
+    
+
+    // print the name Mary tabbed twice
+    
+    
+    // print one, two and three on separate lines using a single line of code
+      
+	
+  }
+  
 }
